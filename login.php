6e26dcfca84eeddfc209cb5d1d06ce04d911303a
Nitzan Bueno <nitzan.bueno@gmail.com>
2016-03-28 22:34:45 +0300

    Fix login.php

--- login.php
+++ login.php
@@ -1,23 +1,29 @@
 <?php
 session_start();
-if(!$_SESSION["username"])
+if($_POST["log"] == "true")
 {
   $_SESSION["username"] = $_POST["username"];
-  header("Location: sqlist.php");
+  header("Location: alglist.php");
   die();
+}
+else
+{
+echo '
+  <!DOCTYPE html>
+  <html>
+   <head>
+     <meta charset="utf-8">
+     <title>Login</title>
+   </head>
+   <body>
+     <form action="login.php" method="post">
+       Username: <input type="text" name="username" /><br/>
+       <input type="hidden" name="log" value="true" />
+       <button type="submit">Login</button>
+     </form>
+   </body>
+  </html>';
+
 }
 
 ?>
-<!DOCTYPE html>
-<html>
- <head>
-   <meta charset="utf-8">
-   <title>Login</title>
- </head>
- <body>
-   <form action="login.php" method="post">
-     Username: <input type="text" name="username" /><br/>
-     <button type="submit">Login</button>
-   </form>
- </body>
-</html>
