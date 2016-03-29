4a82342bb24fb82ee9afc47c19450a59af38b72e
Nitzan Bueno <nitzan.bueno@gmail.com>
2016-03-30 00:54:41 +0300

    Update timestamp support
    Switch from updating LastAsk on 'no' memorization to updating on
    'yes' memorization
    Switch from descending to ascending order on alglist.php

--- alglist.php
+++ alglist.php
@@ -39,7 +39,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT ID, Image FROM Algorithms WHERE Username='" . $sessioname . "' ORDER BY LastAsk DESC";
+$sql = "SELECT ID, Image FROM Algorithms WHERE Username='" . $sessioname . "' ORDER BY LastAsk ASC";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
